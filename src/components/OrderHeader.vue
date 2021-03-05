08f475b7b36ab2c232dd86390c5723cde5ef06f8
niuzhiwei <xujin02@qq.com>
2021-03-05 21:37:27 +0800

    购物车功能完成

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -1,14 +1,74 @@
 <template>
-  <div>order-header</div>
+  <div class="order-header">
+    <div class="container clearfix">
+      <div class="header-logo">
+        <a href="/#/index"></a>
+      </div>
+
+      <div class="title">
+        <h2>{{title}}
+          <slot name='tips'></slot>
+        </h2>
+      </div>
+
+      <div class="username">
+        <a href="javascript:;">{{username}}</a>
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
   name: "order-header",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    ...mapState(["username"]),
+  },
   data() {
     return {};
   },
 };
 </script>
-<style scoped>
+<style lang='scss' scoped>
+.order-header {
+  padding: 30px 0;
+  .container {
+    .header-logo {
+      float: left;
+    }
+    .title,
+    .username {
+      display: inline-block;
+      height: 55px;
+      line-height: 55px;
+    }
+    .title {
+      font-size: 28px;
+      float: left;
+      color: #333;
+      margin-left: 54px;
+      span {
+        font-size: 14px;
+        margin-left: 17px;
+        color: #999;
+        font-weight: bold;
+      }
+    }
+    .username {
+      float: right;
+      a {
+        font-size: 16px;
+        color: #666;
+      }
+    }
+  }
+}
 </style>
